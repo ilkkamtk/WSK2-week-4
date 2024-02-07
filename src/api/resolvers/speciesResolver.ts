1f28360e3de5c70ac2d9d68918c7b616e8d87c64
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-02-07 10:46:44 +0200

    update

--- src/api/resolvers/speciesResolver.ts
+++ src/api/resolvers/speciesResolver.ts
@@ -1,1 +1,16 @@
 // TODO: speciesResolver
+const speciesData = [
+  {
+    id: '1',
+    species_name: 'Cat',
+    category: '1',
+  },
+];
+
+export default {
+  Query: {
+    species: () => {
+      return speciesData;
+    },
+  },
+};
