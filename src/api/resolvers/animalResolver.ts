6c35e29f02aca1b363569b0735d559e222dd2996
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-02-07 10:20:21 +0200

    update

--- src/api/resolvers/animalResolver.ts
+++ src/api/resolvers/animalResolver.ts
@@ -1,1 +1,32 @@
-// TODO: animalResolver
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {Animal} from '../../types/DBTypes';
+const animalData = [
+  {
+    id: '1',
+    animal_name: 'Frank',
+    species: '1',
+  },
+];
+/*
+const speciesData = [
+  {
+    id: '1',
+    species_name: 'Cat',
+    category: '1',
+  },
+];
+
+const categoryData = [
+  {
+    id: '1',
+    category_name: 'Mammal',
+  },
+];
+*/
+export default {
+  Query: {
+    animals: () => {
+      return animalData;
+    },
+  },
+};
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import express, {Request, Response} from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import {notFound, errorHandler} from './middlewares';
-import {MessageResponse} from './types/MessageTypes'
+import {MessageResponse} from './types/MessageTypes';
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import typeDefs from './api/schemas/index';
+import resolvers from './api/resolvers/index';
 
 const app = express();
 
@@ -15,11 +19,20 @@ const app = express();
         contentSecurityPolicy: false,
       }),
     );
-    app.use(cors());
+    // app.use(cors());
 
     app.get('/', (_req: Request, res: Response<MessageResponse>) => {
       res.send({message: 'Server is running'});
     });
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+
+    await server.start();
+
+    app.use('/graphql', cors(), express.json(), expressMiddleware(server));
 
     app.use(notFound);
     app.use(errorHandler);
